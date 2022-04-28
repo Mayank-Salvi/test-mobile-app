926c282062906d314a13b75ff9735695d9277576
Mayank-Salvi <104557237+Mayank-Salvi@users.noreply.github.com>
2022-04-28 15:20:28 +0530

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home-screen',
+    loadChildren: () => import('./page/home-screen/home-screen.module').then(m => m.HomeScreenPageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
